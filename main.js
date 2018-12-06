14af0125795dab1445544adb75839140c0a18168
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-12-06 23:54:00 +0900

    診断点数を実装

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+  //診断点数
+  var TypeANum = 0;
+  var TypeBNum = 0;
+  var TypeCNum = 0;
+
   function main() {
     g.game.pushScene(TitleScene());
+
   }
 
   function TitleScene() {
-    const scene = new g.Scene({
+    var scene = new g.Scene({
       game: g.game,
       assetIds: [
         "text_image1",
@@ -13,7 +19,7 @@
 
     scene.loaded.add(function () {
 
-      // 背景の白
+      // 基本背景とフォント設定
       var bg = new g.FilledRect({
         scene: scene,
         cssColor: "white",
@@ -56,9 +62,9 @@
       scene.append(start_text);
 
       dot_character1.pointDown.handle(function () {
-      scene.append(dot_character2)
-      scene.remove(dot_character1)
-      dot_character1.modified();
+        scene.append(dot_character2)
+        scene.remove(dot_character1)
+        dot_character1.modified();
       });
       dot_character2.pointDown.handle(function () {
         scene.append(dot_character1)
@@ -72,24 +78,108 @@
       start_text.pointDown.handle(function () {
         g.game.replacescene(Question1Scene());
       });
+
     });
     return TitleScene;
   }
 
   function Question1Scene() {
     var scene = new g.Scene({game: g.game});
-  
-      // 背景の白
-      var bg = new g.FilledRect({
-        scene: scene,
-        cssColor: "white",
-        x: 0,
-        y: 0,
-        width: g.game.width,
-        height: g.game.height
-      });
-      scene.append(bg);
+    scene.loaded.add(function () {
+      // 基本背景とフォント設定
+    var bg = new g.FilledRect({
+      scene: scene,
+      cssColor: "white",
+      x: 0,
+      y: 0,
+      width: g.game.width,
+      height: g.game.height
+    });
+    scene.append(bg);
 
+    var font = new g.DynamicFont({
+      game: g.game,
+      fontFamily: g.FontFamily.SansSerif,
+      size: 15
+    });
+
+    var question1_text = new g.Label({
+      scene: scene,
+      font: font,
+      text: "Q1, 家族との関係は良好だ",
+      fontSize: 30,
+      textColor: "black",
+      x: 100,
+      y: 140
+    });
+      scene.append(question_text);
+
+    var question1_select1 = new g.Label({
+      scene: scene,
+      font: font,
+      text: "あてはまる",
+      fontSize: 20,
+      textColor: "black",
+      x: 200,
+      y: 200
+    });
+    scene.append(question1_select1);
+
+    question1_select1.pointDown.handle(function () {
+        TypeANum += 4;
+        g.game.replacescene(Question2Scene());
+    });    
+
+    var question1_select2 = new g.Label({
+      scene: scene,
+      font: font,
+      text: "どちらかというと当てはまる",
+      fontSize: 20,
+      textColor: "black",
+      x: 200,
+      y: 220
+    });
+    scene.append(question1_select2);
+
+    question1_select2.pointDown.handle(function () {
+        TypeANum += 3;
+        g.game.replacescene(Question2Scene());
+    });     
+
+    var question1_select3 = new g.Label({
+      scene: scene,
+      font: font,
+      text: "どちらかというと当てはまらない",
+      fontSize: 20,
+      textColor: "black",
+      x: 200,
+      y: 240
+    });
+    scene.append(question1_select3);
+
+    question1_select3.pointDown.handle(function () {
+        TypeANum += 2;
+        g.game.replacescene(Question2Scene());
+    });    
+  
+    var question1_select4 = new g.Label({
+      scene: scene,
+      font: font,
+      text: "当てはまらない",
+      fontSize: 20,
+      textColor: "black",
+      x: 200,
+      y: 260
+    });
+    scene.append(question1_select4);
+
+    question1_select4.pointDown.handle(function () {
+        TypeANum += 1;
+        g.game.replacescene(Question2Scene());
+    });
+
+    });
+    return Question1Scene;
   }
 
 module.exports = main;
